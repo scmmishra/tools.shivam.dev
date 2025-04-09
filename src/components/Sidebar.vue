07d9ff951ecb54a41c7c13702c0de72c0f7531d6
Shivam Mishra <scm.mymail@gmail.com>
2025-04-09 11:16:36 +0530

    feat: add category to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { tools } from "../tools";
+import { tools, Category } from "../tools";
+
+const groupedTools = Object.values(Category).map((category) => ({
+  name: category.charAt(0).toUpperCase() + category.slice(1),
+  tools: tools.filter((tool) => tool.category === category),
+}));
 </script>
 <template>
   <aside class="sticky top-0 grid h-screen p-3 pr-0 basis-64">
@@ -17,15 +22,25 @@ import { tools } from "../tools";
         </a>
       </span>
 
-      <nav class="space-y-1 -mx-2">
-        <RouterLink
-          v-for="tool in tools"
-          :to="`/${tool.slug}`"
-          class="block px-2 py-1 text-sm text-gray-600 hover:text-gray-900 hover:bg-gray-50"
-          active-class="text-gray-900 bg-gray-50 outline outline-gray-100"
-        >
-          {{ tool.title }}
-        </RouterLink>
+      <nav class="space-y-6 mt-2">
+        <div v-for="group in groupedTools" :key="group.name" class="space-y-1">
+          <h3
+            class="text-xs font-medium text-gray-400 uppercase tracking-wider mb-1"
+          >
+            {{ group.name }}
+          </h3>
+          <div class="-mx-2 space-y-0.5">
+            <RouterLink
+              v-for="tool in group.tools"
+              :key="tool.slug"
+              :to="`/${tool.slug}`"
+              class="block px-2 py-1 text-sm text-gray-600 hover:text-gray-900 hover:bg-gray-50"
+              active-class="text-gray-900 bg-gray-50 outline outline-gray-100"
+            >
+              {{ tool.title }}
+            </RouterLink>
+          </div>
+        </div>
       </nav>
     </div>
   </aside>
