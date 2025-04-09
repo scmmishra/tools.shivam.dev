3009335262a6f6c9f441a969573e55099c062e1f
Shivam Mishra <scm.mymail@gmail.com>
2025-04-09 20:36:07 +0530

    feat: allow code result

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -9,6 +9,7 @@ const props = defineProps<{
   value?: string;
   placeholder?: string;
   disableCopy?: boolean;
+  is?: "p" | "pre";
   error?: string;
 }>();
 
@@ -41,18 +42,19 @@ const handleCopy = async () => {
       <slot name="action" />
     </div>
     <div class="relative">
-      <p
+      <component
+        :is="is || 'p'"
         class="font-mono px-3 py-2 bg-gray-50 outline outline-gray-200 break-all"
         :class="value ? '' : 'text-gray-400'"
       >
         <slot>
           {{ value || placeholder }}
         </slot>
-      </p>
+      </component>
       <button
         v-if="value && !disableCopy"
         @click="handleCopy"
-        class="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+        class="absolute right-2 top-2 text-gray-500 hover:text-gray-700"
       >
         <Transition
           mode="out-in"
